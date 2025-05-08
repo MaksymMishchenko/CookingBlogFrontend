febc3b956226f25cb88565eb21b2aca14873126a
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-05-08 11:35:04 +0300

    feat: Add router-outlet to AdminLayoutComponent

--- CookingBlogFrontend/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ CookingBlogFrontend/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginPageComponent } from '../../../login-page/login-page.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
