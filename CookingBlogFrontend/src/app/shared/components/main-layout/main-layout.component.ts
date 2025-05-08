9a0a820c457796277f3c449aa1371564587d30fd
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-05-08 14:00:08 +0300

    feat: render layout parts in MainLayoutComponent and add styles

--- CookingBlogFrontend/src/app/shared/components/main-layout/main-layout.component.ts
+++ CookingBlogFrontend/src/app/shared/components/main-layout/main-layout.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { TopSidebarComponent } from '../top-sidebar/top-sidebar.component';
+import { NavComponent } from '../nav/nav.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { PostComponent } from '../post/post.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TopSidebarComponent, NavComponent, SidebarComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
