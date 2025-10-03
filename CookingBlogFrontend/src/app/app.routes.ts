d083d75b2bdd7a5d5381a172a8278f087516457c
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-03 17:10:50 +0300

    feat(app-routes): Add about-page component to routes

--- CookingBlogFrontend/src/app/app.routes.ts
+++ CookingBlogFrontend/src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
-    {path: '', component: MainLayoutComponent, children: [        
-        { path: '', component: HomePageComponent }, 
-        { path: 'post/:id', component: PostPageComponent }
-    ]},
+    {
+        path: '', component: MainLayoutComponent, children: [
+            { path: '', component: HomePageComponent },
+            { path: 'post/:id', component: PostPageComponent },
+            { path: 'about', component: AboutPageComponent }
+        ]
+    },
     {
         path: 'admin',
         loadChildren: () => import('./admin/admin.routes').then(m => m.adminRoutes)
