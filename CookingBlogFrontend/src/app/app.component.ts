b84c2cce8388e7ec3dfb93d528a26baf40761f5f
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-03 17:25:28 +0300

    refactor(ui): update styles and data structures across multiple components

--- CookingBlogFrontend/src/app/app.component.ts
+++ CookingBlogFrontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- CookingBlogFrontend/src/app/home-page/home-page.component.ts
+++ CookingBlogFrontend/src/app/home-page/home-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../node_modules/@angular/router/index";
+import { PostComponent } from "../shared/components/post/post.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [PostComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
