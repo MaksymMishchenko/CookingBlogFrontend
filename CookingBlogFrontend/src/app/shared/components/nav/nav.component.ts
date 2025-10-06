ee165a81f5971fca12229b3ffe1af6c02c3d2276
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-06 12:22:59 +0300

    fix(nav): Ensure mobile menu closes on viewport resize 🐛
    The navigation menu component now observes the tablet breakpoint ((min-width: 35em)) to manage the `isMenuOpen` state. This prevents the mobile menu from staying open when the user increases the browser window size to a tablet or desktop layout.

--- CookingBlogFrontend/src/app/shared/components/nav/nav.component.ts
+++ CookingBlogFrontend/src/app/shared/components/nav/nav.component.ts
@@ -1,6 +1,8 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +11,36 @@ import { RouterModule } from '@angular/router';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy {
+  isMenuOpen = false;
+  private breakpointObserver = inject(BreakpointObserver);
+  private breakpointSubscription!: Subscription;
+
+  private readonly tabletBreakpoint = '(min-width: 35em)';
+
   mainMenu = [
     { label: "Home page", link: "/" },
     { label: "About", link: "/about" },
     { label: "Contact", link: "/contact" }
   ];
 
-  isMenuOpen = false;
+  ngOnInit() {
+    this.breakpointSubscription = this.breakpointObserver.observe(this.tabletBreakpoint)
+      .subscribe(result => {
+        if (result.matches) {
+          this.isMenuOpen = true;
+        } else {
+          this.isMenuOpen = false;
+        }
+      });
+  }
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen
+  }
+
+  ngOnDestroy(): void {
+    this.breakpointSubscription.unsubscribe();
   }
 }
 
