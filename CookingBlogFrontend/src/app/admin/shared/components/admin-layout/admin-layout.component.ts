002ca173d8d46c9f897fd30e4dfad0cb9bcda1a9
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-08 22:30:15 +0300

    feat(admin-layout): Render admin header and nav into the admin layout page. Refactor: Remove unneccessary imports from Admin Layout component.

--- CookingBlogFrontend/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ CookingBlogFrontend/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AdminNavComponent } from "../admin-nav/admin-nav.component";
+import { AdminHeaderComponent } from "../admin-header/admin-header.component";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [AdminHeaderComponent, AdminNavComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
