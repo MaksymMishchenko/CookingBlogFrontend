43bd83ca95adf14697ef693d739b3593add399dc
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-10 20:42:27 +0300

    feat(login-page): Handle successful login and navigate to dashboard

--- CookingBlogFrontend/src/app/admin/login-page/login-page.component.ts
+++ CookingBlogFrontend/src/app/admin/login-page/login-page.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { User } from '../../shared/components/interfaces';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -14,6 +17,10 @@ export class LoginPageComponent implements OnInit {
 
   form!: FormGroup;
 
+  constructor(private auth: AuthService, private router: Router) {
+
+  }
+
   ngOnInit() {
     this.form = new FormGroup({
       username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
@@ -25,9 +32,15 @@ export class LoginPageComponent implements OnInit {
     if (this.form.invalid) {
       return;
     }
+
     const user: User = {
-      userName: this.form.value.userName,
+      userName: this.form.value.username,
       password: this.form.value.password
     }
+    
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+    })
   }
 }
