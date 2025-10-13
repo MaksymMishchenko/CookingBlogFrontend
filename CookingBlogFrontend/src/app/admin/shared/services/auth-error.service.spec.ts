fc50eb1edf61f6469722bef32b840cb943b8be45
MaksymMishchenko <mishchenkomv@hotmail.com>
2025-10-13 22:10:06 +0300

    test(auth-error): Cover AuthErrorService with tests

---
+++ CookingBlogFrontend/src/app/admin/shared/services/auth-error.service.spec.ts
@@ -0,0 +1,21 @@
+import { firstValueFrom } from "rxjs";
+import { AuthErrorService } from "./auth-error.service";
+
+describe('', () => {
+    let authErrorService: AuthErrorService
+    beforeEach(() => {
+        authErrorService = new AuthErrorService();
+    });
+
+    it('should emit error to stream', async () => {
+        const errorMessage = 'Invalid credentials';
+       
+        const errorPromise = firstValueFrom(authErrorService.errors$);
+
+        authErrorService.emitError(errorMessage);
+
+        const error = await errorPromise;
+
+        expect(error).toBe(errorMessage)
+    });
+});
