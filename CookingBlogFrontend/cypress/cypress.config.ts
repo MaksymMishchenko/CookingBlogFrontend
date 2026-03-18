cfb4ff8dd5cbc1d675b435343388d070c95c5032
MaksymMishchenko <mishchenkomv@hotmail.com>
2026-03-18 17:33:48 +0200

    refactor: clean up cypress.config.ts

--- CookingBlogFrontend/cypress/cypress.config.ts
+++ CookingBlogFrontend/cypress/cypress.config.ts
@@ -1,25 +1,3 @@
-// import { defineConfig } from "cypress";
-
-// export default defineConfig({
-//   e2e: {
-//     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-//     baseUrl: "http://localhost:4200",
-//     setupNodeEvents(on, config) {      
-//       const env = require('./env/cypress.env.json');
-//       config.env = { ...config.env, ...env };
-//       return config;
-//     },
-//   },
-
-//   component: {
-//     devServer: {
-//       framework: "angular",
-//       bundler: "webpack",
-//     },
-//     specPattern: "cypress/components/**/*.cy.ts",
-//   },
-// });
-
 import { defineConfig } from "cypress";
 import * as fs from 'fs';
 import * as path from 'path';
